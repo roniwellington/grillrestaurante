0283643dad5ed2743a2a13cf67ab18f3e42b589a
Roni Wellington <roniwellington@gmail.com>
2022-12-23 13:15:54 -0300

    header mobile feito

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+'use strict'
+
+/*********************
+ * loading will be end after document is loaded
+ */
+
+const preloader = document.querySelector("[data-preaload]");
+
+window.addEventListener("load", function (){
+    preloader.classList.add("loaded");
+    document.body.classList.add("loaded")
+})
+
+/****************************
+ * add event listener on multiple elements
+ */
+
+const addEventOnElements = function (elements, eventType, callback){
+    for(let i = 0, len = elements.length; i < len; i++){
+        elements[i].addEventListener(eventType, callback);
+    }
+}
+
+/*-------NavBar--------*/
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function(){
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNavbar)
